f1c44189afb528ea9b88bc4e70b53ef6ee067fd4
Adriel Vinicius <adrielvinicius36@gmail.com>
2024-06-17 20:55:17 -0300

    1.0

---
+++ pagina01.php
@@ -0,0 +1,61 @@
+<?php
+
+?>
+
+
+<style>
+
+body {
+	background-image: linear-gradient(90deg, #E07300, #E63F01);
+}
+
+h1 {
+	text-align: center;
+}
+
+main {
+	
+	width: 500px;
+	height: 620px;
+	margin:  30px 30px 30px 30px;
+	text-aling: center;
+	background-color: white;
+	border-radius: 30px;
+	padding: 20px;
+	box-shadow: 10px 10px 10px #E0002E;
+	}
+	
+p {
+	text-indent: 50px;
+	text-aling: justify;
+}
+
+
+</style>
+<body>
+
+<form action="./index.html">
+	<button> VOLTAR</button>
+</form>
+	
+	<main style="margin:auto;">
+		<h1><strong>Comercial Sete Lagoas<strong></h1>
+		<p>Uma rede brasileira de supermercados atacado-varejista, pertencente ao Grupo regional da cidade de Sete Lagoas. Além da cidade, o modelo foi exportado para a Sao Paulo, Santa Catarina, Pernabuco, Rondonia e Tres Marias, em algumas destas cidades, liderando a rede de vendas no setor <q>atacado-varejista</q>.</p>
+		<h2>Historia</h2>
+		<p>Desde 1975, o Grupo Comercial Sete Lagoas abriu caminhos que convergiram para a transformação no país. A história do Grupo Carrefour começou com um encontro – de ruas, no caso. Foi num ponto em que duas vias se cruzaram no município de Annecy, no sudeste da França, que sua primeira unidade surgiu. O ano era 1959. E o nome do estabelecimento ali fundado foi escolhido justamente por sua localização: Carrefour, que em francês significa cruzamento.</p>
+		<h2>Focado em expandir</h2>
+		<p>O Comercial Sete Lagoas ja é o Maior atacadista da regiao de Sete Lagoas em número de lojas, está presente em mais de 200 cidades distribuídas por todos os estados do Brasil, é líder no segmento de atacarejo (cash & carry) e está em rápida expansão.</p>
+		<h2>Informações adicionais</h2>
+		<p>Endereço: Av. Dr. Renato Azeredo, 2955 - Chácara do Paiva, Sete Lagoas - MG, 35700-312
+Horário de funcionamento: 
+Fecha em breve ⋅ 22:00 ⋅ Abre sex. às 07:00
+Telefone: (31) 3779-6450</p>
+	</main>
+
+
+
+</body>
+
+<?
+
+?>
